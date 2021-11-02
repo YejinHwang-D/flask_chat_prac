944958bda8f54ced4727919771a74254dcdd626f
hyj3463#naver.com <hyj3463@naver.com>
2021-11-02 22:02:18 +0900

    채팅

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+from flask_sqlalchemy import SQLAlchemy
+
+import sys
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+socketio = SocketIO(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+class History2(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.String, nullable=True, default="System")
+    message = db.Column('message', db.String)
+
+#History2.query.delete()
+#db.session.commit()
+
+@app.route('/')
+def index():
+    #msg = History2.query.all()
+    return render_template('index.html')
+
+@socketio.on('my event')
+def handle_my_custom_event(msg, methods=['GET', 'POST']):
+    #name = list(msg.keys())
+    #message = list(msg.values())
+    #message = History2(message=message[0], name=name[0])
+    #db.session.add(message)
+    #db.session.commit()
+
+    
+    print(msg, file=sys.stdout)
+
+    socketio.emit('my response', msg)
+
+
+
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
